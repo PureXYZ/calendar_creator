46a2ef846946a3440c5231ebd66137bc283f97ca
PureXYZ <andydai@live.ca>
2016-05-06 18:23:46 -0400

    Testing uw api

--- calander.py
+++ calander.py
@@ -1,7 +1,5 @@
-import requests
+from uwaterlooapi import UWaterlooAPI
+import pprint
+uw = UWaterlooAPI(api_key="8ab9363c27cf84a3fdf526a89269e81a")
 
-r = requests.get('https://api.github.com/events')
-
-r.encoding = 'ascii'
-
-print r.text
+pprint.pprint(uw.course_schedule("CS", "136")[0]['classes'], width=20)
