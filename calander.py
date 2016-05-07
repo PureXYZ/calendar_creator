a8d78d86212ed167cab23cd3ff68f4020fdc9a1b
PureXYZ <andydai@live.ca>
2016-05-06 20:14:35 -0400

    Added read input from file

--- calander.py
+++ calander.py
@@ -2,4 +2,19 @@ from uwaterlooapi import UWaterlooAPI
 import pprint
 uw = UWaterlooAPI(api_key="8ab9363c27cf84a3fdf526a89269e81a")
 
-pprint.pprint(uw.course_schedule("CS", "136")[0]['classes'], width=20)
+#pprint.pprint(uw.term_course_schedule(1165, "math", 237), width=20)
+
+
+input_file = open("input.txt", "r")
+
+lines = input_file.read().splitlines()
+
+term_num = lines[0]
+
+
+counter = 0
+
+for line in lines:
+	if counter != 0:
+		print line
+	counter += 1
