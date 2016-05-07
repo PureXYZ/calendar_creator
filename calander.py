fe91b870744ffcd0668b97367c9a1392770a7ff9
PureXYZ <andydai@live.ca>
2016-05-07 14:34:25 -0400

    Changed input to console instead of file

--- calander.py
+++ calander.py
@@ -3,30 +3,26 @@ import pprint
 uw = UWaterlooAPI(api_key="8ab9363c27cf84a3fdf526a89269e81a")
 
 
-input_file = open("input.txt", "r")
+term_num = (raw_input("Enter the term number (e.g. Spring 2016 = 1165): ")).strip()
 
-lines = input_file.read().splitlines()
-
-term_num = lines[0].strip()
-
-
-del lines[0]
-
-counter = 0
 
 course_name = []
 course_num = []
 course_section = []
 
-for line in lines:
-	course_name.append((line.strip()[:-3]).strip()[:-3].strip())
-	course_num.append((line.strip()[:-3]).strip()[-3:])
-	course_section.append((line.strip()[-3:]).strip())
-	counter += 1
+
+print ("\nEnter 'done' to finish input\n")
+
+
+while True:
+	course_input = (raw_input("Enter course (e.g. Math 237 in section 1 = Math 237 001): ")).strip()
+	if course_input == "done":
+		break
 	
-print course_name
-print course_num
-print course_section
+	course_name.append((course_input.strip()[:-3]).strip()[:-3].strip())
+	course_num.append((course_input.strip()[:-3]).strip()[-3:])
+	course_section.append((course_input.strip()[-3:]).strip())
+	
 
 for index in range(len(course_name)):
 	name = course_name[index]
@@ -34,6 +30,8 @@ for index in range(len(course_name)):
 	sec = course_section[index]
 	
 	course_info = uw.term_course_schedule(term_num, name, num)
+	pprint.pprint(course_info)
 
-
-
+	for section in course_info:
+		if section["section"][-3:] == sec:
+			pprint.pprint("gg")
