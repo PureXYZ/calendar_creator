1bced9c7a85b20e46feb759972c804a7c06f5a79
PureXYZ <andydai@live.ca>
2016-05-07 00:21:48 -0400

    Finished input

--- calander.py
+++ calander.py
@@ -2,19 +2,38 @@ from uwaterlooapi import UWaterlooAPI
 import pprint
 uw = UWaterlooAPI(api_key="8ab9363c27cf84a3fdf526a89269e81a")
 
-#pprint.pprint(uw.term_course_schedule(1165, "math", 237), width=20)
-
 
 input_file = open("input.txt", "r")
 
 lines = input_file.read().splitlines()
 
-term_num = lines[0]
+term_num = lines[0].strip()
 
+
+del lines[0]
 
 counter = 0
 
+course_name = []
+course_num = []
+course_section = []
+
 for line in lines:
-	if counter != 0:
-		print line
+	course_name.append((line.strip()[:-3]).strip()[:-3].strip())
+	course_num.append((line.strip()[:-3]).strip()[-3:])
+	course_section.append((line.strip()[-3:]).strip())
 	counter += 1
+	
+print course_name
+print course_num
+print course_section
+
+for index in range(len(course_name)):
+	name = course_name[index]
+	num = course_num[index]
+	sec = course_section[index]
+	
+	course_info = uw.term_course_schedule(term_num, name, num)
+
+
+
